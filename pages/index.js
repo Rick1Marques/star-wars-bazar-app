534497b0054002b7e07ab65ce4217a864f9f7d9a
Henrique Marques <henriquedms7@gmail.com>
2023-09-21 11:44:04 +0200

    add link HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // import { starshipsApp } from "@/db/dataProcessing/starships_app";
+// console.log(JSON.stringify(starshipsApp, null, 2));
+import Link from "next/link";
 
 export default function HomePage() {
-  // console.log(JSON.stringify(starshipsApp, null, 2));
-  return <h1>Welcome to the Star Wars Bazar!</h1>;
+  return <Link href="./marketplace">Welcome to the Star Wars Bazar!</Link>;
 }
