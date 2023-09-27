593702713f30cbfb5d465512507d788129e3f038
David von Hennigs <dvhennigs@gmail.com>
2023-09-27 16:51:59 +0200

    fix error in api/users

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,14 +1,10 @@
 import connect from "@/db/connect";
 import User from "@/db/models/User";
 
-
 export default async function handler(request, response) {
   await connect();
   if (request.method === "GET") {
-    const users = await User.find({})
-      .sort({ createdAt: -1 })
-      .populate("listings")
-      .exec();
+    const users = await User.find({}).sort({ createdAt: -1 }).exec();
     response.status(200).json(users);
     return;
   }
