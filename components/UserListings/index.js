67eac85061a78b428ddd56fbe1cf16390a8dd05c
Henrique Marques <henriquedms7@gmail.com>
2023-10-04 11:25:18 +0200

    add buy button as placeholder

--- components/UserListings/index.js
+++ components/UserListings/index.js
@@ -1,6 +1,6 @@
 import useSWR from "swr";
 import Flex from "@/components/Layout/Flex";
-import ListingCard from "../ListingCard";
+import UsersListingCard from "../UsersListingCard";
 
 export default function UserList({ listings, starships }) {
   function starshipURL(id) {
@@ -9,10 +9,16 @@ export default function UserList({ listings, starships }) {
 
   return (
     <>
-      <Flex gap="1.5rem" flexWrap="wrap" direction="column" width="90%">
+      <Flex
+        gap="1.5rem"
+        flexWrap="wrap"
+        direction="column"
+        width="90%"
+        alignItems="center"
+      >
         {listings.map((listing) => {
           return (
-            <ListingCard
+            <UsersListingCard
               {...listing}
               key={listing._id}
               name={starshipURL(listing.starship).name}
---
+++ components/UsersListingCard/index.js
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import styled from "styled-components";
+import Flex from "../Layout/Flex";
+
+const StyledImageWrapper = styled.div`
+  height: 135px;
+  width: 135px;
+  border-radius: 11px;
+  border: 0.5px solid var(--secondary-color);
+  overflow: hidden;
+  position: relative;
+`;
+const StyledStarshipImage = styled(Image)`
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+`;
+
+const StyledParagraph = styled.p`
+  color: var(--secondary-color);
+`;
+
+const StyledBuyButton = styled.button`
+  background-color: #000000;
+  border-radius: 5px;
+  border: 0.3px solid var(--secondary-color);
+  height: 2rem;
+  width: 6.25rem;
+  color: var(--secondary-color);
+`;
+
+export default function UsersListingCard({ _id, name, img, preis }) {
+  return (
+    <Flex height="150px" width="90%" justifyContent="space-between">
+      <StyledImageWrapper>
+        <StyledStarshipImage
+          src={img}
+          alt={name}
+          height={0}
+          width={0}
+          layout="responsive"
+        />
+      </StyledImageWrapper>
+      <Flex width="50%" direction="column" alignItems="center">
+        <StyledParagraph>{name}</StyledParagraph>
+        <StyledParagraph>Price: {preis}</StyledParagraph>
+        <StyledBuyButton>Soon...</StyledBuyButton>
+      </Flex>
+    </Flex>
+  );
+}
