88a0691f92609aa1be45e00442830ec3ec208932
David von Hennigs <dvhennigs@gmail.com>
2023-10-05 15:46:41 +0200

    add submit function

--- components/NewOfferForm/index.js
+++ components/NewOfferForm/index.js
@@ -1,14 +1,26 @@
-export default function NewOfferForm({ credit, id }) {
+import useUser from "@/hooks/useUser";
+
+export default function NewOfferForm({ credit, id, onSubmit, user, starship }) {
+  function _onSubmit(event) {
+    event.preventDefault();
+    const data = Object.fromEntries(new FormData(event.target));
+    onSubmit({
+      ...data,
+      user: user._id,
+      starship: starship._id,
+    });
+  }
   return (
-    <form>
-      <label htmlFor="price">Price: </label>
+    <form onSubmit={_onSubmit}>
+      <label htmlFor="preis">Price: </label>
       <input
         type="number"
-        name="price"
-        id="price"
+        name="preis"
+        id="preis"
         placeholder={credit}
         required
       />
+      <button type="submit">Submit</button>
     </form>
   );
 }
--- pages/api/listings/index.js
+++ pages/api/listings/index.js
@@ -1,7 +1,7 @@
 import connect from "@/db/connect";
 import User from "@/db/models/User";
 import Listing from "@/db/models/Listing";
-import Starship from "@/db/models/Starship";
+
 
 export default async function handler(request, response) {
   await connect();
@@ -14,5 +14,22 @@ export default async function handler(request, response) {
     response.status(200).json(listings);
     return;
   }
+
+  if (request.method === "POST") {
+    try {
+      const listing = await Listing.create(request.body);
+      await User.findByIdAndUpdate(listing.user, {
+        $push: {
+          listings: listing._id,
+        },
+      });
+      response.status(201).json(listing);
+    } catch (error) {
+      console.log("POST /api/listings", error);
+      response.status(500).json({ message: "Error creating listing" });
+    }
+    return;
+  }
+
   response.status(405).json({ message: "Method not allowed" });
 }
