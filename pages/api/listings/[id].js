89c6bac3d47d4a20339aef5789b8947135265677
ping zhu <zhuping817@gmail.com>
2023-10-06 12:07:46 +0200

    add delete button to my selling list

---
+++ pages/api/listings/[id].js
@@ -0,0 +1,26 @@
+import connect from "@/db/connect";
+import Listing from "@/db/models/Listing";
+
+export default async function handler(request, response) {
+  await connect();
+
+  if (request.method === "DELETE") {
+    try {
+      const listing = await Listing.findOneAndDelete({ _id: request.query.id });
+      await User.findByIdAndUpdate(listing.user, {
+        $pull: {
+          listings: listing._id,
+          //   listings:{$in: [listing._id]},
+        },
+      });
+
+      response.status(200).json(listing);
+    } catch (error) {
+      console.log("DELETE /api/listings/:id", error);
+      response.status(500).json({ message: "Error deleting listing" });
+    }
+    return;
+  }
+
+  response.status(405).json({ message: "Method not allowed" });
+}
--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -6,7 +6,7 @@ import Starship from "@/db/models/Starship";
 export default async function handler(request, response) {
   await connect();
   if (request.method === "GET") {
-    const user = await User.findById("651ebee42b4437b6db4f7fb9")
+    const user = await User.findById("651fc98dadf5f690555f661f")
       .sort({ createdAt: -1 })
       .populate("listings")
       .populate("starships")
