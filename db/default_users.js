51d2ed1196c06b095f7551058022d65fd43f9009
Henrique Marques <henriquedms7@gmail.com>
2023-10-07 13:45:44 +0200

    add more users

--- db/default_users.js
+++ db/default_users.js
@@ -11,7 +11,7 @@ export const defaultUsers = [
     name: "Jabba the Hutt",
     credits: 0,
     avatar:
-      "https://lumiere-a.akamaihd.net/v1/images/Jabba-The-Hutt_b5a08a70.jpeg?region=150%2C0%2C900%2C675",
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/e/e7/Jabba_Max.jpg/revision/latest/scale-to-width-down/376?cb=20181120074833",
     listings: [],
     starships: [],
   },
@@ -19,15 +19,111 @@ export const defaultUsers = [
     name: "Nute Gunray",
     credits: 0,
     avatar:
-      "https://lumiere-a.akamaihd.net/v1/images/databank_nutegunray_01_169_9d66ded2.jpeg?region=195%2C0%2C1171%2C878",
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/7/7e/Nute3.jpg/revision/latest/scale-to-width-down/376?cb=20180501025256",
     listings: [],
     starships: [],
   },
   {
-    name: "Star Wars Official Händler",
+    name: "Darth Vader",
     credits: 0,
     avatar:
-      "https://m.media-amazon.com/images/I/81EQuc1SHkL._AC_UF894,1000_QL80_.jpg",
+      "https://static.wikia.nocookie.net/antagonisten/images/0/08/Vader-sw-promo-1.png/revision/latest?cb=20210222091614&path-prefix=de",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Luke Skywalker",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/2/2c/Luke_Sky7.jpg/revision/latest/scale-to-width-down/376?cb=20180123070942",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Chewbacca",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/c/ca/Chewbacca_Max.jpg/revision/latest/scale-to-width-down/376?cb=20181121093319",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Princess Leia",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/8/89/Leia19.jpg/revision/latest/scale-to-width-down/376?cb=20170714065406",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Han solo",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/5/52/Han_Solo_II_Max.jpg/revision/latest/scale-to-width-down/376?cb=20181109232925",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Obi-Wan Kenobi",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/e/e6/Ben_Kenobi_Maximus.jpg/revision/latest/scale-to-width-down/376?cb=20181007060550",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Yoda",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/b/bf/Yoda_Max.jpg/revision/latest/scale-to-width-down/376?cb=20181120072949",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Darth Maul",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/3/3d/Maul_Max_wiki.jpg/revision/latest/scale-to-width-down/376?cb=20181218033301",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Boba Fett",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/0/0d/Boba5.jpg/revision/latest/scale-to-width-down/376?cb=20180418073856",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Ben Solo",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/c/c0/Kylo2.jpg/revision/latest?cb=20180302084917",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Jar Jar Binks",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/c/c7/Jar_Jar.jpg/revision/latest/scale-to-width-down/404?cb=20160613230031",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Maz Kanata",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/c/c0/Maz_Two.jpg/revision/latest/scale-to-width-down/376?cb=20180424031903",
+    listings: [],
+    starships: [],
+  },
+  {
+    name: "Qui-Gon Jinn",
+    credits: 0,
+    avatar:
+      "https://static.wikia.nocookie.net/star-wars-canon-extended/images/4/40/Qui2.jpg/revision/latest/scale-to-width-down/376?cb=20170919015731",
     listings: [],
     starships: [],
   },
