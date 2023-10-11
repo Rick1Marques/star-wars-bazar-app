b9f54add52051429c8f7aec93112b03ff54ba3bc
Henrique Marques <henriquedms7@gmail.com>
2023-10-11 16:41:27 +0200

    nothing

--- components/3Experience/Experience.js
+++ components/3Experience/Experience.js
@@ -2,13 +2,26 @@ import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 
 export default function Experience() {
-  useFrame(() => {});
+  const cubeRef = useRef();
+  const groupRef = useRef();
+  useFrame((state, delta) => {
+    groupRef.current.rotation.y += delta;
+  });
   return (
     <>
-      <mesh position={[1, 1, 1]} rotation-y={Math.PI * 0.5} scale={1.5}>
-        <boxGeometry />
-        <meshBasicMaterial color="yellow" />
-      </mesh>
+      <directionalLight position={[1, 2, 3]} intensity={0.5} />
+      <ambientLight intensity={0.5} />
+      <group ref={groupRef}>
+        <mesh
+          ref={cubeRef}
+          position={[1, 1, 1]}
+          rotation-y={Math.PI * 0.5}
+          scale={1.5}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="yellow" />
+        </mesh>
+      </group>
     </>
   );
 }
