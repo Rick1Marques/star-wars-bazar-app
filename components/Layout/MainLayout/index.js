7cd1a8315ecc963b99c21a6f14f93f17e42861ea
Henrique Marques <henriquedms7@gmail.com>
2023-10-14 12:25:42 +0200

    new background

--- components/Layout/MainLayout/index.js
+++ components/Layout/MainLayout/index.js
@@ -3,6 +3,9 @@ import Nav from "../Nav";
 import Head from "next/head";
 import styled from "styled-components";
 import { Blinker } from "@next/font/google";
+import { Canvas } from "@react-three/fiber";
+import Universe from "@/Utilities";
+import Stars from "@/components/threeJs/components/Stars";
 
 const StyledMain = styled.main`
   margin-bottom: 6rem;
@@ -14,13 +17,27 @@ const blinker = Blinker({
 });
 
 export default function MainLayout({ mainUser, children }) {
+  const { bigGroupRadius } = Universe();
   if (!mainUser._id) {
     return (
       <>
         <Head>
           <title>Star Wars Bazaar</title>
         </Head>
-        <StyledMain className={blinker.className}>{children}</StyledMain>
+        <StyledMain className={blinker.className}>
+          <div className="space-background">
+            <Canvas
+              camera={{
+                fov: 45,
+                position: [0, 0, 5 * bigGroupRadius],
+              }}
+            >
+              <Stars />
+            </Canvas>
+          </div>
+
+          {children}
+        </StyledMain>
       </>
     );
   }
@@ -30,11 +47,22 @@ export default function MainLayout({ mainUser, children }) {
         <title>Star Wars Bazaar</title>
       </Head>
 
-      <StyledMain className={blinker.className}>{children}</StyledMain>
+      <StyledMain className={blinker.className}>
+        <div className="space-background">
+          <Canvas
+            camera={{
+              fov: 45,
+              position: [0, 0, 5 * bigGroupRadius],
+            }}
+          >
+            <Stars />
+          </Canvas>
+        </div>
+        {children}
+      </StyledMain>
       <Flex justifyContent="center">
         <Nav />
       </Flex>
-
     </>
   );
 }
--- styles.js
+++ styles.js
@@ -27,7 +27,7 @@ export default createGlobalStyle`
   body {
     margin: 0;
     color: var(--secondary-color);
-background: var(--primary-color);
+background: #080808;
   }
 
 
@@ -38,7 +38,7 @@ background: var(--primary-color);
     width: 100%;
     height:100%;
     overflow: hidden;
-    background: black;
+    background: transparent;
   }
 
 `;
