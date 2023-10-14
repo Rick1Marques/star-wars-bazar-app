ac2391bab9939e7c810fa23b27eff184e15726b9
Henrique Marques <henriquedms7@gmail.com>
2023-10-14 07:44:31 +0200

    fix bug scroll

--- styles.js
+++ styles.js
@@ -30,16 +30,14 @@ export default createGlobalStyle`
 background: var(--primary-color);
   }
 
-  html, body, #__next, main, .space-background{
+
+  .space-background{
     position:fixed;
     top: 0;
     left: 0;
     width: 100%;
     height:100%;
     overflow: hidden;
-  }
-
-  .space-background{
     background: black;
   }
 
