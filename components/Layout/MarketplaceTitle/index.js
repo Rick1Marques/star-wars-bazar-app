22bb3bc98f1915d944553671c2448588919aae1d
Berna Myumyun <bernamyu@gmail.com>
2023-10-15 11:21:53 +0200

    edit title

--- components/Layout/MarketplaceTitle/index.js
+++ components/Layout/MarketplaceTitle/index.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const StyledMarketplaceTitle = styled.h2`
   font-size: 1.5rem;
-  font-weight: 300;
+  font-weight: 400;
   text-align: center;
   background: linear-gradient(90deg, #8feecc 0%, rgba(215, 218, 53, 0.65) 100%);
   -webkit-background-clip: text;
