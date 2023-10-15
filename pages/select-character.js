873228cb0c3b7f01ddcbfb224fa9a24acc23e5bd
Berna Myumyun <bernamyu@gmail.com>
2023-10-15 10:03:22 +0200

    add styling to select character

--- pages/select-character.js
+++ pages/select-character.js
@@ -5,16 +5,22 @@ import styled from "styled-components";
 
 const StyledTitle = styled.h1`
   margin-top: 2rem;
+  font-size: 1.5rem;
+  text-align: center;
+  background: linear-gradient(90deg, #8feecc 0%, rgba(215, 218, 53, 0.65) 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-weight: 300;
 `;
 
 export default function SelectCharacterPage() {
   const { data: users, isLoading } = useSWR(`/api/users`);
   if (!users || isLoading) {
-    return <h1>Loading...</h1>;
+    return <div>Loading...</div>;
   }
   return (
     <Flex direction="column" alignItems="center" gap="3rem">
-      <StyledTitle>Choose your Character</StyledTitle>
+      <StyledTitle>Choose Your Character</StyledTitle>
       <LoginList users={users} />
     </Flex>
   );
