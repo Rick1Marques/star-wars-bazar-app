54d44e8eaec5d1f284653d4933a7387788ad335c
Berna Myumyun <bernamyu@gmail.com>
2023-10-15 11:19:08 +0200

    edit loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,21 @@
 import GlobalStyle from "../styles";
-import useSWR, { SWRConfig } from "swr";
+import { SWRConfig } from "swr";
 import fetcher from "@/lib/fetcher";
-import { Blinker } from "@next/font/google";
 import MainLayout from "@/components/Layout/MainLayout";
-import styled from "styled-components";
 import useUser from "@/hooks/useUser";
-
 
 export default function App({ Component, pageProps }) {
   const { mainUser } = useUser();
   if (!mainUser) {
-    return "Loading";
+    return <div>Loading</div>;
   }
 
   return (
-    
-      <SWRConfig value={{ fetcher }}>
-        <MainLayout mainUser={mainUser}>
+    <SWRConfig value={{ fetcher }}>
+      <MainLayout mainUser={mainUser}>
         <GlobalStyle />
         <Component {...pageProps} mainUser={mainUser} />
-        </MainLayout>
-      </SWRConfig>
-    
+      </MainLayout>
+    </SWRConfig>
   );
 }
