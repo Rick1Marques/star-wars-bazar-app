096286addabe6a5744e289b13e7b6031d702a3fc
Henrique Marques <henriquedms7@gmail.com>
2023-10-19 10:28:31 +0200

    take 3d models starships out

--- components/threeJs/Experience/Experience.js
+++ components/threeJs/Experience/Experience.js
@@ -3,7 +3,7 @@ import Stars from "../components/Stars";
 import Planets from "../components/Planets";
 import PlanetWithRing from "../components/PlanetWithRing";
 import Galaxy from "../components/Galaxy";
-import Starships from "../components/Starships";
+// import Starships from "../components/Starships";
 import { useFrame } from "@react-three/fiber";
 import { useRef, Suspense } from "react";
 import DeathStar from "../components/DeathStar";
@@ -12,14 +12,14 @@ import Universe from "@/Utilities";
 export default function Experience() {
   const { bigGroupRadius } = Universe();
   const bigGroupRef = useRef();
-  const StarshipsRef = useRef();
+  // const StarshipsRef = useRef();
 
   useFrame(({ clock }) => {
     const elapsedTime = clock.getElapsedTime();
     bigGroupRef.current.rotation.y = elapsedTime * 0.03;
 
-    StarshipsRef.current.rotation.y = elapsedTime * 0.5;
-    StarshipsRef.current.rotation.x = elapsedTime * 0.2;
+    // StarshipsRef.current.rotation.y = elapsedTime * 0.5;
+    // StarshipsRef.current.rotation.x = elapsedTime * 0.2;
   });
 
   return (
@@ -35,12 +35,12 @@ export default function Experience() {
         <Suspense>
           <DeathStar />
         </Suspense>
-
+        {/* 
         <group ref={StarshipsRef}>
           <Suspense>
             <Starships />
           </Suspense>
-        </group>
+        </group> */}
       </group>
     </>
   );
